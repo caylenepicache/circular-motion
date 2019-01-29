02a657fe3a3585a06e0890855d4ffb4dd951b919
unknown <caylenepicache@gmail.com>
2019-01-28 23:03:24 -0800

    finished animation

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -128,6 +128,15 @@ addEventListener('resize', function () {
     init();
 });
 
+//Utility Functions
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
 // Objects
 function Particle(x, y, radius, color) {
     var _this = this;
@@ -136,30 +145,45 @@ function Particle(x, y, radius, color) {
     this.y = y;
     this.radius = radius;
     this.color = color;
-    this.radians = 0;
+    this.radians = Math.random() * Math.PI * 2;
     this.velocity = 0.05;
+    this.distanceFromCenter = randomIntFromRange(50, 120);
+    this.lastMouse = {
+        x: x,
+        y: y
+    };
 
     this.update = function () {
+        var lastPoint = {
+            x: _this.x,
+            y: _this.y
+        };
+
         //MOve these points over time
         _this.radians += _this.velocity;
-        _this.x += x + Math.cos(_this.radians) * 100;
-        _this.y += y + Math.sin(_this.radians) * 100;
+
+        //Drag Effect
+        _this.lastMouse.x += (mouse.x - _this.lastMouse.x) * 0.05;
+        _this.lastMouse.y += (mouse.y - _this.lastMouse.y) * 0.05;
+
+        //Circular Motion
+        _this.x += _this.lastMouse.x + Math.cos(_this.radians) * _this.distanceFromCenter;
+        _this.y += _this.lastMouse.y + Math.sin(_this.radians) * _this.distanceFromCenter;
         //oscillates the points from -100 to 100
         //console.log(Math.cos(this.radians) * 100);
-        _this.draw();
+
+        _this.draw(lastPoint);
     };
-}
 
-undefined.draw = function () {
-    c.beginPath();
-    c.arc(undefined.x, undefined.y, undefined.radius, 0, Math.PI * 2, false);
-    c.fillStyle = undefined.color;
-    c.fill();
-    c.closePath();
-};
-
-Object.prototype.update = function () {
-    this.draw();
+    this.draw = function (lastPoint) {
+        c.beginPath();
+        c.strokeStyle = _this.color;
+        c.lineWidth = _this.radius;
+        c.moveTo(lastPoint.x, lastPoint.y);
+        c.lineTo(_this.x, _this.y);
+        c.stroke();
+        c.closePath();
+    };
 };
 
 // Implementation
@@ -167,8 +191,9 @@ var particles = void 0;
 function init() {
     particles = [];
 
-    for (var i = 0; i < 400; i++) {
-        particles.push(new Particle(canvas.width / 2, canvas.height / 2, 5, 'blue'));
+    for (var i = 0; i < 50; i++) {
+        var radius = Math.random() * 2 + 1;
+        particles.push(new Particle(canvas.width / 2, canvas.height / 2, radius, randomColor(colors)));
     }
     console.log(particles);
 }
@@ -176,7 +201,8 @@ function init() {
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = 'rgba(255,255,255,0.05)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
 
     particles.forEach(function (particle) {
         particle.update();
--- src/canvas.js
+++ src/canvas.js
@@ -15,8 +15,8 @@ const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
 // Event Listeners
 addEventListener('mousemove', event => {
-    mouse.x = event.clientX
-    mouse.y = event.clientY
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
 })
 
 addEventListener('resize', () => {
@@ -26,54 +26,82 @@ addEventListener('resize', () => {
     init()
 })
 
+//Utility Functions
+function randomIntFromRange(min,max) {
+    return Math.floor(Math.random() * (max-min + 1) + min);
+}
+
+function randomColor(colors){
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
 // Objects
 function Particle(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-    this.radians = 0;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.radians = Math.random() * Math.PI * 2;
     this.velocity = 0.05;
+    this.distanceFromCenter = randomIntFromRange(50,120);
+    this.lastMouse = {
+        x: x,
+        y: y
+    }
 
     this.update = () => {
+        const lastPoint = {
+            x: this.x,
+            y: this.y
+        };
+
         //MOve these points over time
         this.radians += this.velocity;
-        this.x += x +  Math.cos(this.radians) * 100;
-        this.y += y +  Math.sin(this.radians) * 100;
+
+        //Drag Effect
+        this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05;
+        this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05;
+        
+
+
+        //Circular Motion
+        this.x += this.lastMouse.x +  Math.cos(this.radians) * this.distanceFromCenter;
+        this.y += this.lastMouse.y +  Math.sin(this.radians) * this.distanceFromCenter;
         //oscillates the points from -100 to 100
         //console.log(Math.cos(this.radians) * 100);
-        this.draw();
+
+        this.draw(lastPoint);
     }
     
-}
+    this.draw = lastPoint => {
+        c.beginPath();
+        c.strokeStyle = this.color;
+        c.lineWidth = this.radius;
+        c.moveTo(lastPoint.x, lastPoint.y);
+        c.lineTo(this.x, this.y);
+        c.stroke();
+        c.closePath()
+    };
+};
 
-this.draw = () => {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-}
-
-Object.prototype.update = function() {
-    this.draw()
-}
 
 // Implementation
 let particles;
 function init() {
-    particles = []
+    particles = [];
 
-    for (let i = 0; i < 400; i++) {
-         particles.push(new Particle(canvas.width / 2, canvas.height / 2, 5, 'blue'));
+    for (let i = 0; i < 50; i++) {
+        const radius = (Math.random() * 2) + 1;
+         particles.push(new Particle((canvas.width / 2), (canvas.height / 2), radius, randomColor(colors)));
     }
-    console.log(particles)
+    console.log(particles);
 }
 
 // Animation Loop
 function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    requestAnimationFrame(animate);
+    c.fillStyle = 'rgba(255,255,255,0.05)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
 
      particles.forEach(particle => {
       particle.update();
